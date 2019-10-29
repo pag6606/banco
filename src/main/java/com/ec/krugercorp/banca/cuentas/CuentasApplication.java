144b0f77b27ceb79bf04a1b4d14e6ce8647cf522
Paul Alarcon <pag66.06@gmail.com>
2019-10-28 20:45:09 -0500

    agregando profile

--- src/main/java/com/ec/krugercorp/banca/cuentas/CuentasApplication.java
+++ src/main/java/com/ec/krugercorp/banca/cuentas/CuentasApplication.java
@@ -2,6 +2,7 @@ package com.ec.krugercorp.banca.cuentas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -9,6 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class CuentasApplication {
 
 	public static void main(String[] args) {
+		//set dev enviroment
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
 		SpringApplication.run(CuentasApplication.class, args);
 	}
 
